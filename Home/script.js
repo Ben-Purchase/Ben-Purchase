010e2e751cb1efbb95b97e6f807de72bebf52f35
Ben-Purchase <benpurchse@outlook.com>
2025-02-13 11:50:23 +0000

    new file:   Home/About/about.html 	new file:   Home/Contact/contact.html 	new file:   Home/Education/education.html 	new file:   Home/Experience/experience.html 	new file:   Home/Media/Photos/Photo_1.jpg 	new file:   Home/Media/Videos/Video_1.mp4 	new file:   Home/Projects/projects.html 	new file:   Home/index.html 	new file:   Home/script.js 	new file:   Home/styles.css 	deleted:    index.html 	deleted:    index.js 	deleted:    style.css

---
+++ Home/script.js
@@ -0,0 +1,48 @@
+// Smooth Scroll Effect
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            let hash = this.hash;
+            document.querySelector(hash).scrollIntoView({ behavior: "smooth" });
+        }
+    });
+});
+
+// Contact Form Validation
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("#contact-form");
+    
+    if (form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            let name = document.querySelector("#name").value;
+            let email = document.querySelector("#email").value;
+            let message = document.querySelector("#message").value;
+            let errorMessage = "";
+
+            if (name.trim() === "") errorMessage += "Name is required.\n";
+            if (!email.includes("@")) errorMessage += "Enter a valid email.\n";
+            if (message.trim() === "") errorMessage += "Message cannot be empty.\n";
+
+            if (errorMessage) {
+                alert(errorMessage);
+            } else {
+                
+                // Email form backend
+                emailjs.sendForm('service_c2qvifk', 'template_cp8wkbd', form, 'MtpsJTPFP2Z87I9Fx')
+                .then(() => {
+                    alert('Message Sent Successfully!');
+                    form.reset();
+                }, (error) => {
+                    alert('Failed to Send Message.');
+                    console.error(error);
+                });
+
+                alert("Message Sent Successfully!");
+                form.reset();
+            }
+        });
+    }
+});
+
